65b681c5c60784d01f72eab801615049b86a932c
raffael-marinho <silvamarinho10@gmail.com>
2025-02-15 20:08:46 -0300

    feat:add user model

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const Sequelize = require("sequelize");
 const configDB = require("../config/database");
 
+const User = require('../models/User')
+
 const connection = new Sequelize(configDB);
+
+Userinit(connection)
 
 module.exports = connection;
