0daaf2fc61f0675f61031817cb4de0eddcf945f8
raffael-marinho <silvamarinho10@gmail.com>
2025-02-14 22:48:58 -0300

    fix:refactor the structure and add the .sequelizerc

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const {Router} = require('express');
 
-const router = express.Router();
+const router = Router();
 
 router.get("/", (req, res) => {
     return res.send("Hello world!")
