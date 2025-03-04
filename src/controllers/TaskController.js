50390118ef4d49b3c2d5e2693f41e6fb73ab9a29
raffael-marinho <silvamarinho10@gmail.com>
2025-03-04 16:53:59 -0300

    feat: add delete function

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -50,4 +50,19 @@ module.exports = {
             return res.status(500).json({ error: error.message });
         }
     },
+
+    async deleteTask(req, res) {
+        const { id } = req.params;
+
+        try {
+            const task = await Task.findByPk(id);
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' });
+            }
+            await task.destroy();
+            return res.status(200).json({ message: 'Task deleted successfully' });
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    }
 }
