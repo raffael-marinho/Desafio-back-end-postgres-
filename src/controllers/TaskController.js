d091cf3c5714165820b238f8a2228dbd8122d294
raffael-marinho <silvamarinho10@gmail.com>
2025-03-04 16:53:31 -0300

    feat: add put function

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -20,7 +20,7 @@ module.exports = {
             return res.status(500).json({ error: error.message });
         }
     },
-    
+
     async getTaskById(req, res) {
         const { id } = req.params;
 
@@ -29,6 +29,22 @@ module.exports = {
             if (!task) {
                 return res.status(404).json({ error: 'Task not found' });
             }
+            return res.status(200).json(task);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    },
+
+    async updateTask(req, res) {
+        const { id } = req.params;
+        const { title, description, status, priority, user_id } = req.body;
+
+        try {
+            const task = await Task.findByPk(id);
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' });
+            }
+            await task.update({ title, description, status, priority, user_id });
             return res.status(200).json(task);
         } catch (error) {
             return res.status(500).json({ error: error.message });
