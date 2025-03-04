2d726fa8e6fd25e502ffb4fc83ea729cc9b80917
raffael-marinho <silvamarinho10@gmail.com>
2025-03-04 17:11:16 -0300

    refactor: adjustment for connection and associates

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 const configDB = require("../config/database");
 const User = require('../models/User');
+const Task = require('../models/Task');
 
 const connection = new Sequelize(configDB);
 
 User.init(connection);
+Task.init(connection);
+
+User.associate(connection.models);
+Task.associate(connection.models);
 
 connection.sync({ alter: true })
     .then(() => {
