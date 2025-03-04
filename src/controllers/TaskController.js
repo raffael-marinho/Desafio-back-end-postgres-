9915bd68a6845be03153b62521907e028b7492bf
raffael-marinho <silvamarinho10@gmail.com>
2025-03-04 16:52:51 -0300

    feat: add getId function

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -20,4 +20,18 @@ module.exports = {
             return res.status(500).json({ error: error.message });
         }
     },
+    
+    async getTaskById(req, res) {
+        const { id } = req.params;
+
+        try {
+            const task = await Task.findByPk(id);
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' });
+            }
+            return res.status(200).json(task);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    },
 }
